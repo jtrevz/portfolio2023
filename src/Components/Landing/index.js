41ccc0f6e32c15e2dfd2b7cea9b3e6ea69e6279c
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-03-16 12:10:32 -0500

    ^cont. w/ car change

--- src/Components/Landing/index.js
+++ src/Components/Landing/index.js
@@ -9,17 +9,23 @@ export default function Landing() {
   return (
     <m.div id="Landing" key="Landing" className="" initial={{y:'100%'}} animate={{y:"0%"}} transition={{duration: 0.75}} exit={{opacity:1}}>
       <Container fluid>
-        <Row className="d-flex align-items-stretch pl-4 pl-lg-5 ">
-          <Col className="d-flex justify-content-start align-items-center pl-lg-5 pl-4 min-vh-100 col-md-12">
-            <div className="pb-5">
-              <h1 id="name">
-                JENNIFER TREVIZO
-              </h1>
-              <h4 id="profession">
-                FULL STACK DEVELOPER
-              </h4>
+        <Row className="d-flex align-items-stretch pl-4 pl-lg-5">
+          <Col className="d-flex justify-content-start align-items-center pl-lg-5 pl-4 min-vh-100 col-9">
+            <div className="">
+              <Row className="">
+                <Col className="col-1 line my-2 mx-1"></Col>
+                <Col className="px-1">
+                  <p id="name">
+                    JENNIFER TREVIZO
+                  </p>
+                  <h4 id="profession">
+                    SOFTWARE DEVELOPER
+                  </h4>
+                </Col>
+              </Row>
             </div>
           </Col>
+          <Col></Col>
         </Row>
       </Container>
     </m.div>
